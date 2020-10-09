5a395e9e7c0c1b78fd3e4c3389d13db384f803a2
Tanmay Jain <71631895+TanmayJain97@users.noreply.github.com>
2020-10-09 22:20:14 +0530

    Add: Stack Operation Functions-Peek, Pop etc

--- test/java/bridgelabz/stacks_queues/StackTest.java
+++ test/java/bridgelabz/stacks_queues/StackTest.java
@@ -1,42 +1,53 @@
 package bridgelabz.stacks_queues;
 
-import static org.junit.Assert.*;
-import org.junit.BeforeClass;
-import org.junit.Test;
+public class StackClass<Key> {
 
-public class StackTest {
+	private LinkedListClass<Key> linkedList;
+	public int stackSize;
 
-	static LinkedListClass<Integer> LinkedList;
-	static private LinkedListNode<Integer> Node1;
-	static private LinkedListNode<Integer> Node2;
-	static private LinkedListNode<Integer> Node3;
-	static private StackClass<Integer> stack;
-
-	@BeforeClass
-	public static void init() {
-		//Passing key value
-		Node1 = new LinkedListNode<Integer>(70);
-		Node2 = new LinkedListNode<Integer>(30);
-		Node3 = new LinkedListNode<Integer>(56);
-		stack = new StackClass<Integer>();
-
-		//Pushing to stack
-		stack.push(Node1);
-		stack.push(Node2);
-		stack.push(Node3);
-
-		//Display Stack
-		stack.showStack();
+	public StackClass() {
+		linkedList=new LinkedListClass<>();
+		stackSize=0;
 	}
-
-	//Checking for linking of nodes
-	@Test
-	public void WhenNode1LinkedToNode2ShouldReturnTrue() {
-		assertTrue((Node2.getNextNode()).equals(Node1));
+	
+	public void push(LinkedListNode<Key> newNode) {
+		linkedList.add(newNode);
+		stackSize++;
 	}
-
-	@Test
-	public void WhenNode2LinkedToNode3ShouldReturnTrue() {
-		assertTrue((Node3.getNextNode()).equals(Node2));
+	
+	public LinkedListNode<Key> peek() {
+		return (LinkedListNode<Key>) linkedList.headNode;
+	}
+	
+	public LinkedListNode<Key> pop() {
+		stackSize--;
+		if (!isEmpty()) return (LinkedListNode<Key>) linkedList.pop();
+		else System.out.println("Stack is empty. Cannot pop.");
+		return new LinkedListNode<Key>(null);
+	}
+	
+	public boolean isEmpty() {
+		if (linkedList.headNode==null) return true;
+		else return false;
+	}
+	
+	public int size() {
+		return stackSize;
+	}
+	
+	public void showStack() {
+		System.out.println("Stack -> ");
+		System.out.println();
+		LinkedListFunction<Key> tempNode=linkedList.headNode;
+		do {
+			System.out.println("|  "+tempNode.getKey()+"  |");
+			if (!tempNode.equals(linkedList.tailNode)) {
+				System.out.println("|   |  |");
+				System.out.println("|   V  |");
+			}
+			tempNode=tempNode.getNextNode();
+		}while(tempNode.getNextNode() != null);
+		System.out.println("|  "+tempNode.getKey()+"  |");
+		System.out.println("--------");
 	}
 }
